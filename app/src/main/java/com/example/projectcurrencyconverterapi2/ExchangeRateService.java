11f49953bdfebd0772fcce0ea516a366ee0e9f48
Letícia Feitoza <lmayuyu11@gmail.com>
2025-08-22 11:54:46 -0300

    Implementa a importação do retrofit na interface do projeto

--- app/src/main/java/com/example/projectcurrencyconverterapi2/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi2/ExchangeRateService.java
@@ -1,4 +1,10 @@
 package com.example.projectcurrencyconverterapi2;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 public interface ExchangeRateService {
+
+    /**
+     */
 }
