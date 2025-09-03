a1bea104fdd50279768ca6c2b66b2285d50266d9
Letícia Feitoza <lmayuyu11@gmail.com>
2025-09-03 08:40:45 -0300

    Declarando um mapa que armazena taxas de conversão em moedas.

--- app/src/main/java/com/example/projectcurrencyconverterapi2/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi2/ExchangeRatesResponse.java
@@ -1,6 +1,19 @@
 // Modelo de dados (DTO - Data Transfer Object) que representa a resposta json da API
 package com.example.projectcurrencyconverterapi2;
 
+import java.util.HashMap; // Importa a classe Map da biblioteca Java, usada para armazenar pares das chave-valor.
+import java.util.Map; // Importa a classe Map da biblioteca Java, usada para armazenar pares chave-valor.
+
 public class ExchangeRatesResponse {
+    // Declara um mapa que armazena taxas de conversão em moedas. (ex. USD, EUR, GBP).
+    // A chave (String) representa o código da Moeda e o valor (Double) representa a taxa de conversão.
+
+    private Map<String, Double> conversion_rates = new HashMap<>();
+    // Metodo público para acessar as taxas de conversão.
+    // Retorna o mapa de taxas de conversão.
+
+    public Map<String, Double> getConversion_rates() {
+        return conversion_rates;
+    }
 
 }
