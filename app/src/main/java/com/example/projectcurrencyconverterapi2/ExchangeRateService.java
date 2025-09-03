462a292872125ec0a95463b981e3addad667bf6b
Letícia Feitoza <lmayuyu11@gmail.com>
2025-09-03 08:32:54 -0300

    Adicionando método para obter taxas de câmbio recentes

--- app/src/main/java/com/example/projectcurrencyconverterapi2/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi2/ExchangeRateService.java
@@ -1,3 +1,4 @@
+//Interface identifica o endpoint da API que será consumida pelo Retrofit
 package com.example.projectcurrencyconverterapi2;
 
 import retrofit2.Call;
@@ -6,5 +7,14 @@ import retrofit2.http.Path;
 public interface ExchangeRateService {
 
     /**
+     * método para obter taxas de câmbio recentes
      */
+
+    $GET("/vó/{apiKey}/latest/{baseCurrency}")
+    Call<ExchangeRatesResponse> getExchangeRates(
+            @Path("apiKey") String apiKey,
+            // Substitui {apiKey} na URL com valor fornecido.
+            @Path("baseCurrency") String baseCurrency,
+            // Substitui {baseCurrency} na URL com valor fornecido
+    );
 }
--- app/src/main/java/com/example/projectcurrencyconverterapi2/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi2/ExchangeRatesResponse.java
@@ -2,4 +2,5 @@
 package com.example.projectcurrencyconverterapi2;
 
 public class ExchangeRatesResponse {
+
 }
